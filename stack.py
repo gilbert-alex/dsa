a8b7d9680e76e7a9e58b2ef0892cb60e430d85a6
gilbert-alex <118496831+gilbert-alex@users.noreply.github.com>
2026-02-22 16:52:02 -0800

    init commit: stack

---
+++ stack.py
@@ -0,0 +1,25 @@
+class Stack:
+    def __init__(self):
+        self._data = []
+
+    def push(self, item):
+        self._data.append(item)
+
+    def pop(self):
+        if self.is_empty():
+            raise IndexError("stack is empty")
+        return self._data.pop()
+
+    def peek(self):
+        if self.is_empty():
+            raise IndexError("stack is empty")
+        return self._data[-1]
+
+    def is_empty(self):
+        return len(self._data) == 0
+
+    def size(self):
+        return len(self._data)
+
+    def __repr__(self):
+        return f"Stack({self._data})"
