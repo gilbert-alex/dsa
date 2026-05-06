9ae136b6bbd80675eb3fa15598d3302773c8e904
gilbert-alex <118496831+gilbert-alex@users.noreply.github.com>
2026-05-06 11:05:00 -0700

    add append method and helper methods to linked list
    - includes unit tests
    - append_slow method traverses the ll and runs in O(n)
    - append_fast intended to insert at O(1) but requires ability to move to
      previous node.

--- 01_arrays_and_linked_lists/linked_list.py
+++ 01_arrays_and_linked_lists/linked_list.py
@@ -1,12 +1,15 @@
+# --- Future DSA Uses --- 
 # Use this as a premitive to implement a LL backed stack using prepend
 # Add a append to allow for a ll backed queue
 
+#TODO: add append method and probably flip tests
+#TODO: make sure that the delete method decrements the len counter
 
 class Node:
 	def __init__(self, value):
 		self.value = value
 		self.next = None
-	
+
 	def __repr__(self):
 		return str(self.value)
 
@@ -14,12 +17,13 @@ class Node:
 class LinkedList:
 	def __init__(self):
 		self.head = None
+		self.tail = None
 		self.length = 0
 
 	def prepend(self, value):
 		new_node = Node(value)
 		self.length += 1
-
+		
 		if not self.head:
 			self.head = new_node
 			return
@@ -27,27 +31,89 @@ class LinkedList:
 		new_node.next = self.head
 		self.head = new_node
 
-	def append(self, value):
-		pass
+	def append_slow(self, value):
+		''' For DSA's sake, this will traverse the list and add to the tail.
+			It's O(n).
+		'''
+		current = self.head
+		
+		if not current:
+			self.head = Node(value)
+			self.length += 1
+			return
+
+		while current:
+			if not current.next:
+				current.next = Node(value)
+				self.length += 1
+				return
+			else:
+				current = current.next
+
+	def append_fast(self, value):
+		''' DO NOT USE -- THIS IS INCOMPLETE
+			Wanting to accomplish insertion at O(1) with a tail pointer.
+			The issue is that the tail pointer will have to be updated \
+			in a few different places (prepend, append_*, delete, ...) and this \
+			will cause tightly coupled methods. My first thought is to isolate \
+			the maintance of the tail pointer to a helper func but...
+			- removing an element means the tail moves to previous node
+			- this is a singly-linked list and does not track previous nodes
+			- therefore, this is only accomplished with O(n) traversal which is pointless
+			- stopping here and moving on to doubly-linked list. 
+		'''
+		new_node = Node(value)
+		self.length += 1
+
+		current = self.head
+
+		if not current: 
+			self.head = new_node
+		else:
+			self.tail = new_node
+
 
 	def delete(self, value):
+		''' Will delete all matched elements in list. '''
 		current = self.head
 		previous = None
 
 		if not current:
 			return
 
-		#[40, 30, 20, 10]
 		while current:
 			if current.value == value:
-				previous.next = current.next
-			else:	
-				previous = current
+				if not previous:
+					# if target is first element in list
+					self.head = current.next
+				else:
+					# if any other element in list
+					previous.next = current.next 
+				self.length -= 1
+			# keep stepping through
+			previous = current
 			current = current.next
-		return	
 
-	def search(self, value):
-		pass
+	def search(self, target):
+		''' Returns zero or a count of the times target is found in the list.
+			I wanted to do something more useful like return an index of the \
+			target's position but that isnt helpful if search is only \ 
+			available in O(n).
+			Without a doubly-linked list I can only linear search.
+			I'll do something more meaningful with this in a double-linked list.
+		'''
+		current = self.head
+		counter = 0
+
+		if not current:
+			return counter
+
+		while current:
+			if current.value == target:
+				counter += 1
+			current = current.next
+
+		return counter
 
 	def get_length(self):
 		return self.length
--- 01_arrays_and_linked_lists/tests/test_linked_lists.py
+++ 01_arrays_and_linked_lists/tests/test_linked_lists.py
@@ -1,5 +1,4 @@
 import pytest
-
 from linked_list import LinkedList
 
 
@@ -12,7 +11,15 @@ def empty_ll():
 def sample_ll():
     ll = LinkedList()
     for i in [10, 20, 30, 40]:
-        ll.prepend(i)
+        ll.append_slow(i)
+    return ll
+
+
+@pytest.fixture
+def sample_ll_with_duplicates():
+    ll = LinkedList()
+    for i in [10, 10, 10, 20, 30, 30]:
+        ll.append_slow(i)
     return ll
 
 
@@ -27,17 +34,59 @@ def test_initialization(empty_ll):
     ([1, 2, 3], [3, 2, 1]),
     ([], []),
 ])
-def test_prepends(values, expected):
+def test_prepend(values, expected):
     ll = LinkedList()
     for v in values:
         ll.prepend(v)
     assert ll.to_list() == expected
 
 
-def test_sample_ll(sample_ll):
-    assert sample_ll.to_list() == [40, 30, 20, 10]
+@pytest.mark.parametrize('values, expected', [
+    ([10], [10]),
+    ([1, 2, 3], [1, 2, 3]),
+    ([], []),
+])
+def test_append_slow(values, expected):
+    ll = LinkedList()
+    for v in values:
+        ll.append_slow(v)
+    assert ll.to_list() == expected
+
+
+'''
+@pytest.mark.parametrize('values, expected', [
+    ([10], [10]),
+    ([1, 2, 3], [1, 2, 3]),
+    ([], []),
+])
+def test_append_fast(values, expected):
+    ll = LinkedList()
+    for v in values:
+        ll.append_fast(v)
+    assert ll.to_list() == expected
+'''
 
 
 def test_delete(sample_ll):
     sample_ll.delete(20)
-    assert sample_ll.to_list() == [40, 30, 10]
+    assert sample_ll.to_list() == [10, 30, 40]
+    assert sample_ll.get_length() == 3
+    for i in sample_ll.to_list():
+        #print(f'deleting {i}')  #debug
+        sample_ll.delete(i)
+    assert sample_ll.to_list() == []
+    assert sample_ll.get_length() == 0
+
+
+def test_delete_first_element(sample_ll):
+    sample_ll.delete(40)
+    assert sample_ll.to_list() == [10, 20, 30]
+
+
+def test_delete_last_element(sample_ll):
+    sample_ll.delete(10)
+    assert sample_ll.to_list() == [20, 30, 40]
+
+
+def test_search(sample_ll_with_duplicates):
+    assert sample_ll_with_duplicates.search(10) == 3
