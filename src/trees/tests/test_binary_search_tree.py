8f9084881802e88f329cf8ac8bb4170022944dc0
gilbert-alex <118496831+gilbert-alex@users.noreply.github.com>
2026-06-15 00:21:24 -0700

    bst: tests recursive insert method

--- src/trees/tests/test_binary_search_tree.py
+++ src/trees/tests/test_binary_search_tree.py
@@ -10,6 +10,16 @@ def bst_only_root():
     return t
 
 
+@pytest.fixture
+def bst_depth_two():
+    t = BinarySearchTree()
+    t.root = BSTNode(20)
+    t.root.left = BSTNode(10)
+    t.root.right = BSTNode(30)
+    t.size += 3
+    return t
+
+
 class TestSetup:
     def test_bst_only_root(self, bst_only_root):
         assert bst_only_root.root.value == 10
@@ -39,32 +49,40 @@ class TestBinarySearchTree:
 
 
 class TestInsertPrivate:
-    def test_create_new_root(self):
+    def test_new_node_for_root(self):
         t = BinarySearchTree()
-        assert t.root == None
-        t.insert(1)
-        assert t.root != None
-        assert t.root.value == 1
+        n = t._insert(t.root, 1)
+        n.value = 1
 
 
     def test_new_left_node(self, bst_only_root):
         t = bst_only_root
-        t.insert(9)
+        t._insert(t.root, 9)
         assert t.root.left.value == 9
+        assert t.root.right == None
 
 
     def test_new_right_node(self, bst_only_root):
         t = bst_only_root
-        t.insert(11)
+        t._insert(t.root, 11)
         assert t.root.right.value == 11
+        assert t.root.left == None
 
 
-    def test_new_node_child_pointers(self, bst_only_root):
+    def test_new_node_null_pointers(self, bst_only_root):
         t = bst_only_root
-        t.insert(1)
+        t._insert(t.root, 1)
         new_node = t.root.left
         assert new_node.left == None
         assert new_node.right == None
+
+
+    def test_new_node_below_root(self, bst_depth_two):
+        t = bst_depth_two
+        target_node = t.root.left
+        t._insert(target_node, 1)
+        assert target_node.left.value == 1
+        assert target_node.right == None
 
 
     def test_insert_duplicate_raises(self, bst_only_root):
@@ -76,7 +94,7 @@ class TestInsertPrivate:
 
 
 class TestInsert:
-    def test_create_new_bst(self):
+    def test_set_root(self):
         t = BinarySearchTree()
         assert t.root == None
         t.insert(1)
