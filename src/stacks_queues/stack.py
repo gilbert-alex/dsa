360fa98748d7116cd6841ff45f3670bf66a1b0bb
gilbert-alex <118496831+gilbert-alex@users.noreply.github.com>
2026-07-28 15:55:55 -0700

    feat(stack): adds type hints

--- src/stacks_queues/stack.py
+++ src/stacks_queues/stack.py
@@ -1,9 +1,10 @@
-from typing import Any
+from typing import TypeVar, Generic, cast
 
 from linked_lists.singly_linked_list import SinglyLinkedList
 
+T = TypeVar('T')
 
-class ListStack:
+class ListStack(Generic[T]):
     '''
     A LIFO stack implemented on a Python list. This structure operates on 
     the n-th index of the list where n is the length - 1. 
@@ -17,10 +18,10 @@ class ListStack:
     '''
 
     def __init__(self) -> None:
-        self._data: list = []
+        self._data: list[T] = []
 
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         '''
         As this implementation appends new items on the right side of the 
         Python list, this string similarly displays the top of the stack 
@@ -30,7 +31,7 @@ class ListStack:
         return f"Stack({self._data!r})"
 
 
-    def __str__(self):
+    def __str__(self) -> str:
         '''
         Reversed string to the more logical display order.
         '''
@@ -38,22 +39,22 @@ class ListStack:
         return f"Top -> {' -> '.join(items)}"
 
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self._data)
 
 
-    def push(self, item: Any) -> None:
+    def push(self, item: T) -> None:
         self._data.append(item)
 
 
-    def pop(self) -> Any:
+    def pop(self) -> T:
         if self.is_empty():
             raise IndexError("stack is empty")
 
         return self._data.pop()
 
 
-    def peek(self) -> Any:
+    def peek(self) -> T:
         if self.is_empty():
             raise IndexError("stack is empty")
 
@@ -64,7 +65,7 @@ class ListStack:
         return len(self._data) == 0
 
 
-class LinkedListStack:
+class LinkedListStack(Generic[T]):
     '''
     A LIFO stack implemented on a singly linked list. This structure operates on 
     the 0-th index of the list.
@@ -72,11 +73,11 @@ class LinkedListStack:
     This representation of a stack operates at O(1) time complexity for both 
     insertion and deletion operations.
     '''
-    def __init__(self):
+    def __init__(self) -> None:
         self._data = SinglyLinkedList()
 
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         '''
         The base linked list builds with a prepend method such that the 
         top of the Stack is always at the Linked List head.
@@ -85,20 +86,20 @@ class LinkedListStack:
         return f"Stack([{', '.join(items)}])"
 
 
-    def __str__(self):
+    def __str__(self) -> str:
         items = [repr(v) for v in self._data.to_list()]
         return f"Top -> {' -> '.join(items)}"
 
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self._data)
 
 
-    def push(self, item):
+    def push(self, item: T) -> None:
         self._data.prepend(item)
         
 
-    def pop(self):
+    def pop(self) -> T:
         '''
         Implementing a LinkedListStack.delete_head() would be best.
         This method relies on SinglyLinkedList.delete() which is intended to 
@@ -107,16 +108,18 @@ class LinkedListStack:
         if self.is_empty():
             raise IndexError('Stack is empty')
 
-        top = self.peek()
+        top: T = self.peek()
         self._data.delete(top)
         return(top)
 
 
-    def peek(self):
+    def peek(self) -> T:
         if self.is_empty():
             raise IndexError('Stack is empty')
 
-        return self._data.head.value
+        head = self._data.head
+        assert head is not None
+        return head.value
 
 
     def is_empty(self) -> bool:
@@ -126,7 +129,7 @@ class LinkedListStack:
 if __name__ == "__main__":
     # list backed stack debug
     print('ListStack:')
-    ls = ListStack()
+    ls: ListStack[object] = ListStack()
     for v in [1, 2, 3, '4', 'five', '', 10]:
         ls.push(v)
     print(ls)
@@ -138,7 +141,7 @@ if __name__ == "__main__":
     print('---')
     # linked list backed stack debug
     print('LinkedListStack:')
-    lls = LinkedListStack()
+    lls: LinkedListStack[object] = LinkedListStack()
     for v in [10, 20, 30, '40', 'fifty', '', 100]:
         lls.push(v)
     print(lls)
